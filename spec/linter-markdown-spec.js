24d84062b165feaefa89291e0cbeafa97e0fa7d0
Titus Wormer <tituswormer@gmail.com>
2016-07-22 17:27:36 +0200

    Add support for ignore files, local remark-lint
    *  Update remark to version 5.0.0;
    *  Use `unified-engine-atom` to deal with processing,
       this adds support for `.remarkignore` files, local remark-lint
       configuration files.
    
    ---
    
    Closes GH-71
    
    `remark-lint` specified in `.remarkrc` or `package.json` files now
    precedes over the packaged `remark-lint`.
    
    ---
    
    Closes GH-83.
    Closes GH-76.
    
    Using `unified-engine`, which comes with pretty JSON errors.

--- spec/linter-markdown-spec.js
+++ spec/linter-markdown-spec.js
@@ -39,8 +39,9 @@ describe('The remark-lint provider for Linter', () => {
           expect(messages[0].type).toEqual('Error');
           expect(messages[0].html).toBeDefined();
           expect(messages[0].html).toEqual(
-            '<span class="badge badge-flexible">definition-case</span> ' +
-            'Do not use upper-case characters in definition labels'
+            '<span class="badge badge-flexible">remark-lint:' +
+            'definition-case</span> Do not use upper-case ' +
+            'characters in definition labels'
           );
           expect(messages[0].filePath).toBeDefined();
           expect(messages[0].filePath).toMatch(/.+definition-case-invalid\.md$/);
