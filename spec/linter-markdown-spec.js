16c4faae6d719ad2e05c4392d2219d7135ee38ee
Landon Abney <Landonabney@gmail.com>
2017-01-24 12:38:14 -0800

    Cleanup and update specs
    There were some duplications in the specs being ran, as well as a minor
    change from `Error` to `Warning` needed.

--- spec/linter-markdown-spec.js
+++ spec/linter-markdown-spec.js
@@ -34,18 +34,13 @@ describe('The remark-lint provider for Linter', () => {
     it('verifies the first message', () => {
       waitsForPromise(() =>
         lint.provideLinter().lint(editor).then((messages) => {
-          expect(messages[0].type).toBeDefined();
-          expect(messages[0].type).toEqual('Error');
-          expect(messages[0].html).toBeDefined();
+          expect(messages[0].type).toEqual('Warning');
+          expect(messages[0].text).not.toBeDefined();
           expect(messages[0].html).toEqual(
             '<span class="badge badge-flexible">remark-lint:' +
             'no-unused-definitions</span> Found unused definition'
           );
-          expect(messages[0].filePath).toBeDefined();
           expect(messages[0].filePath).toMatch(/.+definition-use-invalid\.md$/);
-          expect(messages[0].range).toBeDefined();
-          expect(messages[0].range.length).toBeDefined();
-          expect(messages[0].range.length).toEqual(2);
           expect(messages[0].range).toEqual([[2, 0], [2, 58]]);
         })
       );
@@ -57,7 +52,7 @@ describe('The remark-lint provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(validPath).then(editor =>
         lint.provideLinter().lint(editor).then(messages =>
-          expect(messages.length).toEqual(0)
+          expect(messages.length).toBe(0)
         )
       )
     );
